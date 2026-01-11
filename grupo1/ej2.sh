dfc9a235de8b130665978da348b0d54b86c20dc9
MaarcosDevs <maheru@alumnatflorida.es>
2026-01-11 16:24:23 +0100

    ej2

--- grupo1/ej2.sh
+++ grupo1/ej2.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-while true; do
-    echo "Introduce un número mayor que 0:"
-    read num
+echo "Introduce un número mayor que 0:"
+read num
 
-    if [[ "$num" =~ ^[0-9]+$ ]] && [ "$num" -gt 0 ]; then
-        if [ $((num % 2)) -eq 0 ]; then
-            echo "El número es par"
-        else
-            echo "El número es impar"
-        fi
-        break
-    else
-        echo "Valor incorrecto. Inténtalo de nuevo."
-    fi
+while [ $num -le 0 ]; do
+    echo "Número incorrecto. Introduce otro:"
+    read num
 done
+
+if [ $((num % 2)) -eq 0 ]; then
+    echo "El número es par"
+else
+    echo "El número es impar"
+fi
